c58c5c6207a8573364463ad74bcea601d9791157
Eric Lipert Kalisz <erickalisz@gmail.com>
2020-07-06 09:55:34 +1000

    fixed week list alignment

--- components/WeekItem.js
+++ components/WeekItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-
 import { myStyles } from '../assets/helper/Styles';
 import { stringVal, stringPer } from '../assets/constants/strings';
 import { SetPrecision, setWeekString } from '../assets/helper/helper';
@@ -9,68 +8,104 @@ const WeekItem = (props) => {
 
     const { selectedWeek, column } = props;
 
-    let week = setWeekString(selectedWeek.start, selectedWeek.end);
+    const week = setWeekString(selectedWeek.start, selectedWeek.end);
 
-    let Del = SetPrecision(selectedWeek.deliveroo);
-    let Uber = SetPrecision(selectedWeek.uber);
-    let Hours = SetPrecision(selectedWeek.hours);
-    let Total = SetPrecision(selectedWeek.total);
-    let Per = SetPrecision(selectedWeek.per);
-    let Acc = selectedWeek.accurate;
+    const Del = SetPrecision(selectedWeek.deliveroo);
+    const Uber = SetPrecision(selectedWeek.uber);
+    const Hours = SetPrecision(selectedWeek.hours);
+    const Total = SetPrecision(selectedWeek.total);
+    const Per = SetPrecision(selectedWeek.per);
+    const Acc = selectedWeek.accurate;
 
-    let text = '';
+    //Formatted string values
+
+    const sDel = stringVal(Del);
+    const sUber = stringVal(Uber);
+    const sTotal = stringVal(Total);
+    const sPer = stringPer(Per);
+
+    let firstValue = '-', secondValue = '-';
+    let shouldDisplay = true;
 
     if (column === 'week') {//default sorting
         if (Hours === 0 && Total < 1) {//not worked
-            text = week + ' - Not worked';
+            //do nothing
         }
 
         else if (Hours === 0 && Total > 0 || !Acc) {//worked but don't know the hours
-            text = week + stringVal(Total);
+            firstValue = sTotal;
         }
 
-        else text = week + stringVal(Total) + stringPer(Per);
+        else { //all data valid
+            firstValue = sTotal;
+            secondValue = sPer;
+        }
     }
     else if (column === 'deliveroo') {//sorting by deliveroo
 
         if (!Acc || (Hours < 1 && Del > 0)) {//not accurate or no hours recorded at all
-            text = week + stringVal(Del);
+            firstValue = sDel;
         }
         else if (Hours > 0 && Del > 0) {// everything set
-            text = week + stringVal(Del) + stringPer(Per);
+            firstValue = sDel;
+            secondValue = sPer;
         }
-        else text = '-1';
+        else shouldDisplay = false;
     }
     else if (column === 'uber') {//sorting by uber
 
         if (!Acc || (Hours < 1 && Uber > 0)) {//not accurate or no hours recorded at all
-            text = week + stringVal(Uber);
+            firstValue = sUber;
         }
         else if (Hours > 0 && Uber > 0) {// everything set
-            text = week + stringVal(Uber) + stringPer(Per);
+            firstValue = sUber;
+            secondValue = sPer;
         }
-        else text = '-1';
+        else shouldDisplay = false;
     }
     else if (column === 'total') {//sorting by total
 
         if (!Acc || (Hours < 1 && Total > 0)) {//not accurate or no hours recorded at all
-            text = week + stringVal(Total);
+            firstValue = sTotal;
         }
         else if (Hours > 0 && Total > 0) {// everything set
-            text = week + stringVal(Total) + stringPer(Per);
+            firstValue = sTotal;
+            secondValue = sPer;
         }
-        else text = '-1';
+        else shouldDisplay = false;
     }
     else if (column === 'per') {//sorting by per
         if (Acc && Hours > 0 && Per > 0) {// everything set
-            text = week + stringVal(Total) + stringPer(Per);
+            firstValue = sTotal;
+            secondValue = sPer;
         }
-        else text = '-1';
+        else shouldDisplay = false;
     }
+    else shouldDisplay = false;
 
-    return text === '-1' ? (null) : (
+    return !shouldDisplay ? (null) : (
         <View style={myStyles.listItem}>
-            <Text style={myStyles.listItemValue}>{text}</Text>
+            <View style={{ flex: 9, display: 'flex', flexDirection: 'row' }}>
+
+                <View style={{ flex: 3 }}>
+                    <Text style={myStyles.listItemValue}>
+                        {week}
+                    </Text>
+                </View>
+
+                <View style={{ flex: 2 }}>
+                    <Text style={myStyles.listItemValue}>
+                        {firstValue}
+                    </Text>
+                </View>
+
+                <View style={{ flex: 2 }}>
+                    <Text style={myStyles.listItemValue}>
+                        {secondValue}
+                    </Text>
+                </View>
+
+            </View>
         </View>
     );
 }
